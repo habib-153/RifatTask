b7710389770aeebfdd6f419d1e9990c86f948abf
habib-153 <h.r.sihab155@gmail.com>
2024-11-23 21:51:00 +0600

    update API base URLs for production deployment and adjust CORS settings

--- client/src/redux/api/baseApi.ts
+++ client/src/redux/api/baseApi.ts
@@ -5,19 +5,19 @@ import {
   FetchArgs,
   createApi,
   fetchBaseQuery,
-} from '@reduxjs/toolkit/query/react';
-import { RootState } from '../store';
-import { logout, setUser } from '../features/auth/authSlice';
-import { toast } from 'sonner';
+} from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
+import { logout, setUser } from "../features/auth/authSlice";
+import { toast } from "sonner";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'http://localhost:5000/api/v1',
-  credentials: 'include',
+  baseUrl: "https://auto-attend-server.vercel.app/api/v1",
+  credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
 
     if (token) {
-      headers.set('authorization', `${token}`);
+      headers.set("authorization", `${token}`);
     }
 
     return headers;
@@ -31,20 +31,23 @@ const baseQueryWithRefreshToken: BaseQueryFn<
 > = async (args, api, extraOptions): Promise<any> => {
   let result = await baseQuery(args, api, extraOptions);
 
-  if(result?.error?.status === 404){
+  if (result?.error?.status === 404) {
     toast.error((result?.error?.data as { message: string })?.message);
   }
-  if(result?.error?.status === 403){
+  if (result?.error?.status === 403) {
     toast.error((result?.error?.data as { message: string })?.message);
   }
   if (result?.error?.status === 401) {
     //* Send Refresh
-    console.log('Sending refresh token');
+    console.log("Sending refresh token");
 
-    const res = await fetch('http://localhost:5000/api/v1/auth/refresh-token', {
-      method: 'POST',
-      credentials: 'include',
-    });
+    const res = await fetch(
+      "https://auto-attend-server.vercel.app/api/v1/auth/refresh-token",
+      {
+        method: "POST",
+        credentials: "include",
+      }
+    );
 
     const data = await res.json();
 
@@ -68,8 +71,8 @@ const baseQueryWithRefreshToken: BaseQueryFn<
 };
 
 export const baseApi = createApi({
-  reducerPath: 'baseApi',
+  reducerPath: "baseApi",
   baseQuery: baseQueryWithRefreshToken,
-  tagTypes: ['users'],
+  tagTypes: ["users"],
   endpoints: () => ({}),
 });
